f81d2843a6e4c5fdb545d47679957aadee1d8d43
arnhav <arnhavcam@gmail.com>
2025-09-05 21:17:43 -0500

    Fix conversations not firing for SimpleNPCs

--- src/main/java/org/betonquest/betonquest/compatibility/npc/simplenpcs/SimpleIdentifier.java
+++ src/main/java/org/betonquest/betonquest/compatibility/npc/simplenpcs/SimpleIdentifier.java
@@ -4,16 +4,16 @@ import com.ags.simplenpcs.objects.SNPC;
 import org.betonquest.betonquest.compatibility.npc.GenericReverseIdentifier;
 
 /**
- * Allows to get NpcIds for a FancyNpcs Npc.
+ * Allows to get NpcIds for a SimpleNPCs Npc.
  */
 public class SimpleIdentifier extends GenericReverseIdentifier<SNPC> {
 
     /**
-     * Create a new Fancy Identifier.
+     * Create a new Simple Identifier.
      *
      * @param prefix the prefix of relevant Ids
      */
     public SimpleIdentifier(final String prefix) {
-        super(prefix, SNPC.class, original -> String.valueOf(original.getId()));
+        super(prefix.toLowerCase(), SNPC.class, original -> String.valueOf(original.getId()));
     }
 }
